8d09b5b7b1512a1ba0427c5718ac0b051dc4a41a
ebao <ebao@toolsgroup.com>
2020-02-09 17:06:02 +0100

    add svm prediction and atlx compatable lexicon output

--- scripts/03_svm_classifier.py
+++ scripts/03_svm_classifier.py
@@ -1,7 +1,12 @@
 from sklearn.svm import LinearSVC
 from sklearn.model_selection import GridSearchCV
 import pandas as pd
+from gensim.models import Word2Vec
+import numpy as np
 
+
+# TRAIN
+# -----
 
 df = pd.read_csv('data/processed/seed_vectors.csv', index_col='SEED')
 
@@ -24,11 +29,35 @@ svc = LinearSVC(C=c, verbose=1, random_state=42, max_iter=5000)
 svc.fit(X, y)
 
 
-x = X[1,:].reshape(1, -1)
+# x = X[1,:].reshape(1, -1)
+#
+# svc.predict(x)
+#
+# svc.decision_function(x)
 
-svc.predict(x)
 
-svc.decision_function(x)
+# PREDICT
+# -------
+lx_df = pd.read_csv('data/processed/lexicon_table_v2.csv', index_col='WORD')
+lx_words = lx_df.index.tolist() # 13297
 
+w2vec_model = Word2Vec.load('data/raw/amazon/Electronics.bin')
+vocabs = set(w2vec_model.wv.index2entity) # 43750
+score_words = [w for w in lx_words if w in vocabs] # 5687
+score_vectors = np.concatenate([w2vec_model[w].reshape(1,-1) for w in score_words])
+
+prediction = svc.predict(score_vectors)
+score_df = pd.DataFrame(prediction.reshape(-1,1), index=pd.Series(score_words, name='WORD'), columns=['DALX'])
+
+# make atlx compatable
+score_df = lx_df.join(score_df)
+score_df.DALX.replace(0, -1, inplace=True)
+
+for idx,row in score_df.iterrows():
+    dalx_val = row.DALX
+    if not np.isnan(dalx_val):
+        score_df.loc[idx] = dalx_val
+
+score_df.to_csv('data/output/lexicon_table_dalx.csv')
 
 
