26632df68e9f3e60c74e2b1c9b6d864f9f2dadae
ebao <ebao@toolsgroup.com>
2020-02-25 11:09:37 +0100

    add util functions

---
+++ src/utils/__init__.py
@@ -0,0 +1,24 @@
+import yaml
+import pandas as pd
+
+
+def save_yaml(dict, path, mode='w'):
+    with open(path, mode) as outfile:
+        yaml.dump(dict, outfile, default_flow_style=False)
+
+def read_yaml(yaml_file):
+    with open(yaml_file, 'r') as ymlfile:
+        yml = yaml.load(ymlfile)
+    return yml
+
+def load_semeval15_laptop(train, test):
+    _train = pd.read_csv(train)
+    _train['IS_TRAIN'] = True
+    _test = pd.read_csv(test)
+    _test['IS_TRAIN'] = False
+    return pd.concat([_train, _test], ignore_index=True)
+
+
+def search_keyword(df, key: str, col: str = 'SENT'):
+    in_sent = df[col].apply(lambda s: True if key in s.split() else False)
+    return df.loc[in_sent]
